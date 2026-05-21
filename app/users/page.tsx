824f4c66230bf5fab57286ee2cc614d2e9273569
unknown <funmilolaadebiyi52@gmail.com>
2026-05-21 07:26:44 +0100

    new ui-updates

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import TableClient from "./components";
 
 
@@ -6,7 +7,9 @@ export default function TablePage() {
 
   return (
     <div className="container mx-auto py-10 font-inter">
-      <TableClient/>
+      <Suspense fallback={<div className="p-4 text-center">Loading table data...</div>}>
+        <TableClient />
+        </Suspense>
     </div>
   );
 }
